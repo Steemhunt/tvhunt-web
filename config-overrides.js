23051c262359d809729460f38b6f5228866b6843
ggomaeng <sungwoopark95@gmail.com>
2019-11-09 02:47:01 -0800

    Configure react hot loader for hot-reloading

--- config-overrides.js
+++ config-overrides.js
@@ -1,16 +1,21 @@
-const { override, fixBabelImports, addLessLoader } = require('customize-cra');
+const { override, fixBabelImports, addLessLoader } = require("customize-cra");
+const rewireReactHotLoader = require("react-app-rewire-hot-loader");
 
 module.exports = override(
-  fixBabelImports('import', {
-    libraryName: 'antd',
-    libraryDirectory: 'es',
+  fixBabelImports("import", {
+    libraryName: "antd",
+    libraryDirectory: "es",
     style: true
   }),
+  (config, env) => {
+    config = rewireReactHotLoader(config, env);
+    return config;
+  },
   addLessLoader({
     javascriptEnabled: true,
     modifyVars: {
       "@primary-color": "#ff54ae",
-      "@font-family": "'Open Sans', sans-serif",
+      "@font-family": "'Open Sans', sans-serif"
     }
   })
 );
--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Routes from "./Routes";
+import { hot } from 'react-hot-loader/root'
 import './custom.css';
 
 function App() {
   return <Routes />;
 }
 
-export default App;
+export default process.env.NODE_ENV === "development" ? hot(App) : App
