ab2e247872f23b77221af90ee6b8aa805ab33339
Sebastian Kim <sydneyitguy@gmail.com>
2019-11-18 21:16:49 +0900

    #34 - Fix indentation

--- src/components/MetaHelmet.js
+++ src/components/MetaHelmet.js
@@ -3,56 +3,49 @@ import Helmet from "react-helmet";
 import PropTypes from "prop-types";
 
 const MetaHelmet = props => {
-     const {
-          title,
-          description,
-          image,
-          url,
-          script,
-          onChangeClientState
-     } = props;
-     return (
-          <Helmet script={script} onChangeClientState={onChangeClientState}>
-               <title>{title}</title>
-               {/* Search Engine */}
-               <meta name="description" content={description} />
-               <meta name="image" content={image} />
-               {/* Schema.org for Google */}
-               <meta itemprop="name" content={title} />
-               <meta itemprop="description" content={description} />
-               <meta itemprop="image" content={image} />
-               {/* Twitter */}
-               <meta name="twitter:card" content="summary_large_image" />
-               <meta name="twitter:title" content={title} />
-               <meta name="twitter:description" content={description} />
-               <meta name="twitter:image:src" content={image} />
-               <meta name="twitter:site" content="@steemhunt" />
-               <meta name="twitter:creator" content="@steemhunt" />
-               {/* Open Graph general (Facebook, Pinterest & Google+) */}
-               <meta property="og:title" content={title} />
-               <meta property="og:description" content={description} />
-               <meta property="og:image" content={image} />
-               <meta property="og:url" content={url} />
-               <meta property="og:site_name" content="Tvhunt" />
-               <meta property="og:type" content="website" />
-          </Helmet>
-     );
+  const { title, description, image, url, script, onChangeClientState } = props;
+  return (
+    <Helmet script={script} onChangeClientState={onChangeClientState}>
+      <title>{title}</title>
+      {/* Search Engine */}
+      <meta name="description" content={description} />
+      <meta name="image" content={image} />
+      {/* Schema.org for Google */}
+      <meta itemprop="name" content={title} />
+      <meta itemprop="description" content={description} />
+      <meta itemprop="image" content={image} />
+      {/* Twitter */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image:src" content={image} />
+      <meta name="twitter:site" content="@steemhunt" />
+      <meta name="twitter:creator" content="@steemhunt" />
+      {/* Open Graph general (Facebook, Pinterest & Google+) */}
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={image} />
+      <meta property="og:url" content={url} />
+      <meta property="og:site_name" content="Tvhunt" />
+      <meta property="og:type" content="website" />
+    </Helmet>
+  );
 };
 
 MetaHelmet.propTypes = {
-     title: PropTypes.string,
-     description: PropTypes.string,
-     image: PropTypes.string,
-     url: PropTypes.string
+  title: PropTypes.string,
+  description: PropTypes.string,
+  image: PropTypes.string,
+  url: PropTypes.string
 };
 
 MetaHelmet.defaultProps = {
-     title: "TV Hunt - Daily top chart for videos",
-     description: "TV Hunt is a daily top chart for videos. Anyone can simply share a cool video that they found today. No sign-ups are required.",
-     image: `${process.env.REACT_APP_PUBLIC_URL}/og-image-1200.png`,
-     url: process.env.REACT_APP_PUBLIC_URL,
-     script: [],
-     onChangeClientState: () => {}
+  title: "TV Hunt - Daily top chart for videos",
+  description: "TV Hunt is a daily top chart for videos. Anyone can simply share a cool video that they found today. No sign-ups are required.",
+  image: `${process.env.REACT_APP_PUBLIC_URL}/og-image-1200.png`,
+  url: process.env.REACT_APP_PUBLIC_URL,
+  script: [],
+  onChangeClientState: () => {}
 };
 
 export default MetaHelmet;
