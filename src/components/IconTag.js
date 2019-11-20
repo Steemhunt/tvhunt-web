8c5d209c16545a8f23581a27fada07a485a79f92
ggomaeng <sungwoopark95@gmail.com>
2019-11-20 10:46:10 -0800

    #37 - Tag clickable

--- src/components/IconTag.js
+++ src/components/IconTag.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 const IconTag = props => {
   const { style, src, text } = props;
   return (
-    <div className="icon-tag" style={style}>
+    <div className="icon-tag hover-link" style={style}>
       <img src={src} alt="" />
       <div>{text}</div>
     </div>
