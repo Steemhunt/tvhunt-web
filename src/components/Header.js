99171b87b23e54adff6096e213a4d7b7925845aa
Sebastian Kim <sydneyitguy@gmail.com>
2019-11-24 19:25:22 +0900

    Add PH banner

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,6 +19,17 @@ const Header = props => {
       <a href="/">
         <img className="header-logo" src={logo} alt="LOL Hunt Logo" />
       </a>
+
+      <a href="https://www.producthunt.com/posts/lol-hunt?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-lol-hunt" target="_blank">
+        <img
+          src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=175492&theme=dark" 
+          alt="LOL Hunt - Daily hunt for funny Youtube clips, powered by Blockstack | Product Hunt Embed" 
+          style={{
+            width: '250px',
+            height: '54px'
+          }}
+        />
+      </a>
     </div>
   );
 };
