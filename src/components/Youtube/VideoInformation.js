4a614d7968b6f50981acc359475b7266c4c85c57
ggomaeng <sungwoopark95@gmail.com>
2019-12-04 22:01:17 -0800

    Override youtube controls

--- src/components/Youtube/VideoInformation.js
+++ src/components/Youtube/VideoInformation.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { Icon } from "antd";
+import { Icon, notification } from "antd";
 import VideoContext from "contexts/VideoContext";
 import SubmitContext from "contexts/SubmitContext";
 import IconTag from "components/IconTag";
@@ -23,12 +23,45 @@ const VideoInformation = props => {
         "mobile"} fade-in-out ${(hover || status === STATUS_PAUSED) &&
         "hover"}`}
     >
-      <div className="row-align-center">
-        {currentVideo && (
+      {currentVideo && (
+        <>
+          <div className="info-container">
+            <div className="row-space-between text-white title-share">
+              <a
+                href={`https://youtube.com/watch?v=${currentVideo.unique_id}`}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="title-text hover-link"
+              >
+                {currentVideo.title}
+              </a>
+              <div className="row-align-center">
+                <a
+                  className="youtube-button hover-link mobile-portrait-hidden"
+                  href={`https://youtube.com/watch?v=${currentVideo.unique_id}`}
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <Icon type="youtube" />
+                </a>
+                <Icon
+                  className="share-button hover-link mobile-portrait-hidden"
+                  type="share-alt"
+                  onClick={() => {
+                    navigator.clipboard.writeText(window.location.href);
+                    notification["success"]({ message: "Copied to clipboard" });
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+
           <div className="row-align-center badge small">
             <img src={badge} alt="" />
             <div>
-              <div className="secondary">Hunted by {displayUsername(currentVideo, 'a guest user')}</div>
+              <div className="secondary">
+                Hunted by {displayUsername(currentVideo, "a guest user")}
+              </div>
               <div className="text">
                 {moment(currentVideo.created_at).fromNow()}
               </div>
@@ -44,8 +77,8 @@ const VideoInformation = props => {
               <div>{numeral(currentVideo.vote_count).format("0,0")}</div>
             </div>
           </div>
-        )}
-      </div>
+        </>
+      )}
       <div className="row-align-center tags">
         {player &&
           currentVideo &&
